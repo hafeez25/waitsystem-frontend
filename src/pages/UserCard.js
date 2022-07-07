8f1280bf83e7f67718961e11d735b3abb3fe24e0
Sagar Gupta <sagarkumargupta8@gmail.com>
2022-07-07 10:12:52 +0530

    UserCard added

--- src/pages/UserCard.js
+++ src/pages/UserCard.js
@@ -1,7 +1,53 @@
-import React from 'react'
+import React from 'react';
+import {Grid,Card,Avatar,CardHeader,CardActions,Button} from '@mui/material';
+import Page from '../components/Page';
+
 
 export const UserCard = () => {
   return (
-    <div>UserCard</div>
+    <Page title="User Card">
+      <Grid container spacing={3}>
+        <Grid item md={4}>
+      <Card sx={{backgroundColor:'#D1E9FC'}}>
+        <CardHeader avatar = {<Avatar sx={{height: '100px', width: '100px', marginBottom:'20px'}} src="https://images.unsplash.com/photo-1512917774080-9991f1c4c750?auto=format&w=350&dpr=2" alt="UserPhoto"/>}title="Sagar Gupta (Admin)" subheader="Roorkee, Uttrakhand"/>
+        <CardActions>
+        <Button size="medium" color="primary" sx={{marginLeft:"9rem"}}>
+          View Profile
+        </Button>
+      </CardActions>
+      </Card>
+      </Grid>
+      <Grid item md={4}>
+      <Card sx={{backgroundColor:'#D1E9FC'}}>
+        <CardHeader avatar = {<Avatar sx={{height: '100px', width: '100px', marginBottom:'20px'}} src="https://images.unsplash.com/photo-1512917774080-9991f1c4c750?auto=format&w=350&dpr=2" alt="UserPhoto"/>}title="Sagar Gupta" subheader="Dehradun, Uttrakhand"/>
+        <CardActions>
+        <Button size="medium" color="primary" sx={{marginLeft:"9rem"}}>
+          View Profile
+        </Button>
+      </CardActions>
+      </Card>
+      </Grid>
+      <Grid item md={4}>
+      <Card sx={{backgroundColor:'#D1E9FC'}}>
+        <CardHeader avatar = {<Avatar sx={{height: '100px', width: '100px', marginBottom:'20px'}} src="https://images.unsplash.com/photo-1512917774080-9991f1c4c750?auto=format&w=350&dpr=2" alt="UserPhoto"/>}title="Sagar Sharma" subheader="Gwalior, Madhya Pradesh"/>
+        <CardActions>
+        <Button size="medium" color="primary" sx={{marginLeft:"9rem"}}>
+          View Profile
+        </Button>
+      </CardActions>
+      </Card>
+      </Grid>
+      <Grid item md={4}>
+      <Card sx={{backgroundColor:'#D1E9FC'}}>
+        <CardHeader avatar = {<Avatar sx={{height: '100px', width: '100px', marginBottom:'20px'}} src="https://images.unsplash.com/photo-1512917774080-9991f1c4c750?auto=format&w=350&dpr=2" alt="UserPhoto"/>}title="Sagar Mishra" subheader="Roorkee, Uttrakhand"/>
+        <CardActions>
+        <Button size="medium" color="primary" sx={{marginLeft:"9rem"}}>
+          View Profile
+        </Button>
+      </CardActions>
+      </Card>
+      </Grid>
+      </Grid>
+    </Page>
   )
 }
